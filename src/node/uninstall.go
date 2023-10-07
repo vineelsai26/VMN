d05ae18e3f4e24f90bab9fdcf1224dc0ac1b295c
Vineel Sai <mail@vineelsai.com>
2023-10-07 12:50:04 +0530

    add node use and uninstall tests

--- src/node/uninstall.go
+++ src/node/uninstall.go
@@ -3,36 +3,46 @@ package node
 import (
 	"fmt"
 	"os"
+	"strings"
 
 	"vineelsai.com/vmn/src/utils"
 )
 
-func uninstallVersion(version string) {
+func uninstallVersion(version string) (string, error) {
 	if !utils.IsInstalled(version, "node") {
 		panic("Node.js version " + version + " is not installed")
 	}
 	fmt.Printf("Uninstalling Node.js %s\n", version)
 	path, err := utils.GetVersionPath(version, "node")
 	if err != nil {
-		panic(err)
+		return "", err
 	}
 	os.RemoveAll(path)
+
+	return "Node.js version " + version + " uninstalled successfully", nil
 }
 
-func Uninstall(version string) {
+func Uninstall(version string) (string, error) {
+	version = strings.TrimPrefix(version, "v")
 	if version == "all" {
 		for _, version := range GetAllVersions() {
-			uninstallVersion(version)
+			if _, err := uninstallVersion(version); err != nil {
+				return "", err
+			}
 		}
 	} else if version == "lts" {
 		for _, version := range GetAllLTSVersions() {
-			uninstallVersion(version)
+			if _, err := uninstallVersion(version); err != nil {
+				return "", err
+			}
 		}
 	} else if version == "latest" {
-		uninstallVersion(GetLatestVersion())
+		version = "v" + GetLatestVersion()
 	} else if version != "" {
-		uninstallVersion(version)
+		version = "v" + version
 	} else {
-		panic("Invalid version")
+		return "", fmt.Errorf("invalid version")
 	}
+
+	return uninstallVersion(version)
 }
--- tests/node_test.go
+++ tests/node_test.go
@@ -1,13 +1,43 @@
 package tests
 
 import (
+	"os"
+	"path/filepath"
 	"testing"
 
 	"vineelsai.com/vmn/src/node"
+	"vineelsai.com/vmn/src/utils"
 )
 
 func TestNode_20_Install(t *testing.T) {
 	msg, err := node.Install("20")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Log(msg)
+}
+
+func TestNode_20_Use(t *testing.T) {
+	version, err := node.Use("20")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Log(version)
+
+	content, err := os.ReadFile(filepath.Join(utils.GetHome(), ".vmn", "node-version"))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if string(content) != filepath.Join(utils.GetHome(), ".vmn", "node", "v"+version, "bin") {
+		t.Fatal("invalid version")
+	}
+}
+
+func TestNode_20_Uninstall(t *testing.T) {
+	msg, err := node.Uninstall("20")
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -24,6 +54,33 @@ func TestNode_18_Install(t *testing.T) {
 	t.Log(msg)
 }
 
+func TestNode_18_Use(t *testing.T) {
+	version, err := node.Use("18")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Log(version)
+
+	content, err := os.ReadFile(filepath.Join(utils.GetHome(), ".vmn", "node-version"))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if string(content) != filepath.Join(utils.GetHome(), ".vmn", "node", "v"+version, "bin") {
+		t.Fatal("invalid version")
+	}
+}
+
+func TestNode_18_Uninstall(t *testing.T) {
+	msg, err := node.Uninstall("18")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Log(msg)
+}
+
 func TestNode_16_Install(t *testing.T) {
 	msg, err := node.Install("16")
 	if err != nil {
@@ -33,8 +90,62 @@ func TestNode_16_Install(t *testing.T) {
 	t.Log(msg)
 }
 
+func TestNode_16_Use(t *testing.T) {
+	version, err := node.Use("16")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Log(version)
+
+	content, err := os.ReadFile(filepath.Join(utils.GetHome(), ".vmn", "node-version"))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if string(content) != filepath.Join(utils.GetHome(), ".vmn", "node", "v"+version, "bin") {
+		t.Fatal("invalid version")
+	}
+}
+
+func TestNode_16_Uninstall(t *testing.T) {
+	msg, err := node.Uninstall("16")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Log(msg)
+}
+
 func TestNode_14_Install(t *testing.T) {
 	msg, err := node.Install("14")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Log(msg)
+}
+
+func TestNode_14_Use(t *testing.T) {
+	version, err := node.Use("14")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Log(version)
+
+	content, err := os.ReadFile(filepath.Join(utils.GetHome(), ".vmn", "node-version"))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if string(content) != filepath.Join(utils.GetHome(), ".vmn", "node", "v"+version, "bin") {
+		t.Fatal("invalid version")
+	}
+}
+
+func TestNode_14_Uninstall(t *testing.T) {
+	msg, err := node.Uninstall("14")
 	if err != nil {
 		t.Fatal(err)
 	}
